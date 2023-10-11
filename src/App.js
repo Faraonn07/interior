0c6a070010b7794bcc1ee701e00098d6e925ddd1
Faraonn07 <125273194+Faraonn07@users.noreply.github.com>
2023-10-11 17:18:43 +0300

    actions

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Second Test online push
+          test
         </a>
       </header>
     </div>
