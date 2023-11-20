f926310b0b97655c41d59a067d9d76693b964a88
OnderSanek <saneklife2003@gmail.com>
2023-11-20 15:30:14 +0200

    add Login Register pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,5 @@
-
 import React, { Component } from 'react';
-import { Button, Container,  Nav, Form, Navbar } from 'react-bootstrap';
+import { Button, Container, Nav, Form, Navbar } from 'react-bootstrap';
 import Logo1 from "./Logo1.jpg";
 import '../components/Header.css'
 import "../components/RedactorBtn.css"
@@ -12,63 +11,57 @@ import HomePage from './HomePage';
 import Login from "./Login"
 import Register from "./Register"
 
-
-
-
-
 export default class Header extends Component {
   render() {
     return (
       <>
         <Navbar className='navbar' fixed='top' collapseOnSelect  expand="md" bg="dark" variant="dark">
-            <Container>
-                <Navbar.Brand href="/">
-                    <img
-                        src={Logo1}
-                        height="30"
-                        width="30"
-                        className='d-inline-block align-top'
-                        alt='Logo'
-                    />   
-                </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-                    <Navbar.Collapse id="responsive-navbar-nav" > 
-                    <Nav className='mr-auto'>
-                        <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="/redactor">Redactor</Nav.Link>
-                        <Nav.Link href="/about">About us</Nav.Link>
-                        <Nav.Link href="/contacts">Contacts</Nav.Link>
-                    </Nav>
-                    <Form className="d-flex">
+          <Container>
+            <Navbar.Brand href="/">
+              <img
+                src={Logo1}
+                height="30"
+                width="30"
+                className='d-inline-block align-top'
+                alt='Logo'
+              />   
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+            <Navbar.Collapse id="responsive-navbar-nav" > 
+              <Nav className='mr-auto'>
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/redactor">Redactor</Nav.Link>
+                <Nav.Link href="/about">About us</Nav.Link>
+                <Nav.Link href="/contacts">Contacts</Nav.Link>
+              </Nav>
+              <div className="d-flex"> {/* Зміна тут: обгорнув Form та Button в контейнер d-flex */}
                 <Form.Control 
-                     type="text"
-                     placeholder="Search"
-                     className="mr-sm-2"
-                    aria-label="Search"
+                  type="text"
+                  placeholder="Search"
+                  className="mr-sm-2"
+                  aria-label="Search"
                 />
                 <Button variant="outline-success">Search</Button>
-                </Form>
-                <Nav className='mr-auto'>
-                        
-                        <Nav.Link href="/login">Login</Nav.Link>
-                        <Nav.Link href="/register">Register</Nav.Link>
-                    </Nav>
-                    </Navbar.Collapse>
-            </Container>
+              </div>
+              <Nav className='mr-auto'>
+                <Nav.Link href="/login">Login</Nav.Link>
+                <Nav.Link href="/register">Register</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
         </Navbar>
         
-       <Router>
-        <Switch>
+        <Router>
+          <Switch>
             <Route exact path='/' component={HomePage} />
             <Route exact path='/redactor' component={Redactor} />
             <Route exact path='/about' component={About} />
             <Route exact path='/contacts' component={Contacts} />
             <Route exact path='/login' component={Login} />
             <Route exact path='/register' component={Register} />
-        </Switch>
-       </Router>
+          </Switch>
+        </Router>
       </>
     );
   }
 }
-
